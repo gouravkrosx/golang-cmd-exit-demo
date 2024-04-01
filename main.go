fedea011e23cebaf11ee137f10823280ecc02362
gouravkrosx <gouravgreatkr@gmail.com>
2024-04-01 13:07:25 +0530

    sync code with blog

--- main.go
+++ main.go
@@ -49,15 +49,17 @@ func main() {
 
 func run(ctx context.Context, logger *zap.Logger, userCmd string) error {
 	// Run the app as the user who invoked sudo
-	username := os.Getenv("SUDO_USER")
+	// username := os.Getenv("SUDO_USER")
+
+	// cmd := exec.CommandContext(ctx, "sh", "-c", userCmd)
+	// if username != "" {
+	// 	// print all environment variables
+	// 	logger.Debug("env inherited from the cmd", zap.Any("env", os.Environ()))
+	// 	// Run the command as the user who invoked sudo to preserve the user environment variables and PATH
+	// 	cmd = exec.CommandContext(ctx, "sudo", "-E", "-u", os.Getenv("SUDO_USER"), "env", "PATH="+os.Getenv("PATH"), "sh", "-c", userCmd)
+	// }
 
 	cmd := exec.CommandContext(ctx, "sh", "-c", userCmd)
-	if username != "" {
-		// print all environment variables
-		logger.Debug("env inherited from the cmd", zap.Any("env", os.Environ()))
-		// Run the command as the user who invoked sudo to preserve the user environment variables and PATH
-		cmd = exec.CommandContext(ctx, "sudo", "-E", "-u", os.Getenv("SUDO_USER"), "env", "PATH="+os.Getenv("PATH"), "sh", "-c", userCmd)
-	}
 
 	// Set the cancel function for the command
 	cmd.Cancel = func() error {
